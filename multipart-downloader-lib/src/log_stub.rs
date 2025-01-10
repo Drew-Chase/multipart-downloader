b4ba7ec08f85f320fea1bc55b23d5d2307e0ddc8
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-10 15:50:26 -0500

    Remove unnecessary `log_stub` module wrapper.
    The `log_stub` module wrapper was redundant as the macros are already exported independently. This change simplifies the codebase without altering functionality.

--- multipart-downloader-lib/src/log_stub.rs
+++ multipart-downloader-lib/src/log_stub.rs
@@ -1,19 +1,19 @@
-#[cfg(not(feature = "log"))]
-mod log_stub {
-    #[macro_export]
-    macro_rules! debug {
-        ($($arg:tt)*) => {};
-    }
-    #[macro_export]
-    macro_rules! info {
-        ($($arg:tt)*) => {};
-    }
-    #[macro_export]
-    macro_rules! warn {
-        ($($arg:tt)*) => {};
-    }
-    #[macro_export]
-    macro_rules! error {
-        ($($arg:tt)*) => {};
-    }
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {};
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {};
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {};
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {};
 }
