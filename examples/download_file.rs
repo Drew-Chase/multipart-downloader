21a44821bd62c9fba85793663a37b6075c2dd3c3
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-01-10 19:10:42 -0500

    Refactor download logic for improved clarity and defaults
    Simplified the DownloadClient configuration by adjusting defaults for parts and preallocation while reducing method chaining. Updated download target path handling for improved readability. Minor formatting changes applied for better code clarity.

--- examples/download_file.rs
+++ examples/download_file.rs
@@ -4,17 +4,14 @@ use multipart_downloader_lib::download_client::DownloadClient;
 #[tokio::main]
 async fn main() -> Result<()> {
     let mut client = DownloadClient::new();
-    client
-        .with_parts(10)
-        .with_concurrent_downloads(10)
-        .set_preallocate_space(false);
+    client.with_parts(50).set_preallocate_space(false);
     match client
         .download(
             "https://bungi.mm.fcix.net/ubuntu-releases/24.04.1/ubuntu-24.04.1-desktop-amd64.iso",
-            "./",
-            Some("ubuntu.iso".to_string()),
+            "./ubuntu.iso",
             |progress| {
-                let percentage = (progress.bytes_downloaded as f64 / progress.total_bytes as f64) * 100.0;
+                let percentage =
+                    (progress.bytes_downloaded as f64 / progress.total_bytes as f64) * 100.0;
                 let mb_per_sec = progress.bytes_per_second as f64 / 1024.0 / 1024.0;
                 println!("Downloading {:.2}% {:.2}MB/s", percentage, mb_per_sec);
             },
