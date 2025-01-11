ce743939ec07e57474a73ae6bf24ba822664a10e
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-01-11 00:29:10 -0500

    Add async file downloader with progress tracking support
    Replaced the greet function with an async download function that supports progress updates via IPC. Integrated multiple plugins to enhance functionality, including HTTP, filesystem, SQL, and dialogs. This improves the app's capability to handle multipart downloads efficiently.

--- multipart-downloader-app/src-tauri/src/lib.rs
+++ multipart-downloader-app/src-tauri/src/lib.rs
@@ -1,14 +1,38 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use multipart_downloader_lib::download_client::DownloadProgress;
+use tauri::ipc::Channel;
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn download(
+    url: String,
+    parts: u16,
+    allocate: bool,
+    filepath: String,
+    on_event: Channel<DownloadProgress>,
+) -> Result<(), String> {
+    let mut client = multipart_downloader_lib::download_client::DownloadClient::new();
+    client.with_parts(parts);
+    client.set_preallocate_space(allocate);
+
+    client
+        .download(url, filepath, move |progress| {
+            on_event.send(progress).unwrap_or_else(|err| {
+                eprintln!("Error sending progress: {:?}", err);
+            });
+        })
+        .await
+        .map_err(|err| format!("Download failed: {:?}", err))?;
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![download])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
