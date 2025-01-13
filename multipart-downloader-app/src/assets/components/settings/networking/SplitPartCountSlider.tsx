3f233bf52356105d7b8ef882b63a781abc0b92b6
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-13 15:36:44 -0500

    Add custom input field and value marks to slider
    Enhanced the `SplitPartCountSlider` component by introducing a `PSInput` field to render and update slider values dynamically. Added labeled marks at specific intervals (1, 16, 128, 255) for improved user interaction and accessibility. These changes provide a more user-friendly and customizable slider experience.

--- multipart-downloader-app/src/assets/components/settings/networking/SplitPartCountSlider.tsx
+++ multipart-downloader-app/src/assets/components/settings/networking/SplitPartCountSlider.tsx
@@ -1,6 +1,7 @@
 import {Slider} from "@nextui-org/react";
+import PSInput from "../../variants/PSInput.tsx";
 
-export default function SplitPartCountSlider({value, onValueChange}: { value: number, onValueChange: (value: number) => void})
+export default function SplitPartCountSlider({value, onValueChange}: { value: number, onValueChange: (value: number) => void })
 {
     return (
         <Slider
@@ -14,6 +15,34 @@ export default function SplitPartCountSlider({value, onValueChange}: { value: nu
             size={"sm"}
             className={"w-full"}
             onChange={value => onValueChange(value as number)}
+            marks={[
+                {
+                    value: 1,
+                    label: "1"
+                },
+                {
+                    value: 16,
+                    label: "16"
+                },
+                {
+                    value: 128,
+                    label: "128"
+                },
+                {
+                    value: 255,
+                    label: "255"
+                }
+            ]}
+            renderValue={() =>
+                <PSInput
+                    value={value.toString()}
+                    className={"w-12"}
+                    classNames={{
+                        input: "text-center",
+                        inputWrapper: "!bg-background-L200"
+                    }}
+                    onValueChange={v => onValueChange(+v.replace(/[^0-9]/g, ""))}
+                />}
         />
     );
 }
