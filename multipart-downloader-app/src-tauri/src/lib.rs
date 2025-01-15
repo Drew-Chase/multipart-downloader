8a5cf8e94d73670aaf86d80ebbc026b573ed7db6
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-15 16:15:04 -0500

    Add system tray support and single instance plugin
    Refactored the code to include a system tray with a quit option, enhancing UX. Integrated the `tauri-plugin-single-instance` plugin to prevent multiple app instances. Adjusted dependencies and modularized commands for better maintainability.

--- multipart-downloader-app/src-tauri/src/lib.rs
+++ multipart-downloader-app/src-tauri/src/lib.rs
@@ -1,45 +1,40 @@
-use multipart_downloader_lib::download_client::DownloadProgress;
-use tauri::ipc::Channel;
+use anyhow::Result;
+use commands::{download, get_default_downloads_directory, try_get_filename};
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
+use tauri::{App, Manager};
 
-#[tauri::command]
-async fn download(
-    url: String,
-    parts: u16,
-    allocate: bool,
-    filepath: String,
-    on_event: Channel<DownloadProgress>,
-) -> Result<(), String> {
-    let mut client = multipart_downloader_lib::download_client::DownloadClient::new();
-    client.with_parts(parts);
-    client.set_preallocate_space(allocate);
-
-    client
-        .download(url, filepath, move |progress| {
-            on_event.send(progress).unwrap_or_else(|err| {
-                eprintln!("Error sending progress: {:?}", err);
-            });
-        })
-        .await
-        .map_err(|err| format!("Download failed: {:?}", err))?;
-
-    Ok(())
-}
-
-#[tauri::command]
-async fn get_default_downloads_directory() -> Result<String, String> {
-    dirs::download_dir()
-        .map(|path| path.to_string_lossy().to_string())
-        .ok_or_else(|| "Failed to determine the default downloads directory".to_string())
-}
+mod commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+            let window = app.get_webview_window("main").expect("no main window");
+            window.set_focus().expect("Failed to focus on window");
+        }))
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(tauri::generate_handler![download, get_default_downloads_directory])
+        .setup(|app| {
+            build_system_tray(app)?;
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            download,
+            get_default_downloads_directory,
+            try_get_filename
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn build_system_tray(app: &mut App) -> Result<()> {
+    let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&quit_i])?;
+    let _ = TrayIconBuilder::new().menu(&menu).build(app)?;
+    Ok(())
 }
