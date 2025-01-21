e63e7bd8f1f130936714dc4e781617456378813a
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-21 12:54:06 -0500

    Refactor Home page to remove hardcoded download items
    Commented out the mock data initialization for download items to improve maintainability. Updated the `DownloadsTable` component to no longer depend on predefined items, making the component more reusable and dynamic.

--- multipart-downloader-app/src/assets/pages/Home.tsx
+++ multipart-downloader-app/src/assets/pages/Home.tsx
@@ -1,35 +1,35 @@
-import Download from "../ts/download.ts";
 import DownloadsTable from "../components/DownloadsTable.tsx";
 import ListActions from "../components/ListActions.tsx";
 
 export default function Home()
 {
-   const download_items = Array.from({length: 100}, (_, i) => i).map(_ =>
-       {
-           const url: string = "https://bungi.mm.fcix.net/ubuntu-releases/24.04.1/ubuntu-24.04.1-desktop-amd64.iso";
-           const filename: string = "C:\\Users\\drew_\\Downloads\\ubuntu.iso";
-           const bytes_per_second: string = "180.12 MB/s";
-           const bytes_downloaded: string = "2.00 GB";
-           const total_bytes: string = "5.75 GB";
-           const parts_downloaded: number = 16;
-           const parts_total: number = 20;
-           const progress: number = 0.5;
-           return new Download(
-               url,
-               filename,
-               bytes_downloaded,
-               total_bytes,
-               parts_downloaded,
-               parts_total,
-               bytes_per_second,
-               progress
-           );
-       });
+    // const downloadItems = Array.from({length: 100}, (_, i) => i).map(id =>
+    // {
+    //     const url: string = `https://example.com/file-${id}.iso`;
+    //     const filename: string = `C:\\Users\\drew_\\Downloads\\file-${id}.iso`;
+    //     const bytes_per_second: number = Math.random() * (200 * 1024 * 1024 - 100 * 1024 * 1024) + 100 * 1024 * 1024;
+    //     const bytes_downloaded: number = Math.random() * (5.75 * 1024 * 1024 * 1024);
+    //     const total_bytes: number = 5.75 * 1024 * 1024 * 1024;
+    //     const parts_downloaded: number = Math.floor(Math.random() * 20) + 1;
+    //     const parts_total: number = 20;
+    //     const status: DownloadStatus = [DownloadStatus.Idle, DownloadStatus.Downloading, DownloadStatus.Paused, DownloadStatus.Completed, DownloadStatus.Error][Math.floor(Math.random() * 5)];
+    //     return {
+    //         id,
+    //         url,
+    //         filename,
+    //         bytes_downloaded,
+    //         total_bytes,
+    //         parts_downloaded,
+    //         parts_total,
+    //         bytes_per_second,
+    //         status
+    //     } as DownloadItem;
+    // });
 
     return (
         <div className={"flex flex-col p-4 w-full h-full"}>
-            <ListActions />
-            <DownloadsTable downloads={download_items}/>
+            <ListActions/>
+            <DownloadsTable/>
         </div>
     );
 }
